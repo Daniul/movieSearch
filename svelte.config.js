ce619d54c358a7924a4bb2f999436d0012e5b00d
danieldavid0901@gmail.com <60299227+Daniul@users.noreply.github.com>
2022-07-13 14:52:33 -0500

    pls work

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,13 @@ import adapter from '@sveltejs/adapter-auto';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		vite: {
+            define: {
+                'process.env': process.env,
+            },
+        },
+
 	}
 };
 
